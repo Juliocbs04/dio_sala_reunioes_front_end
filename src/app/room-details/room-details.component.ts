a2e83cc5dc62768ab8a4a02345e2d8a7f9467359
Juliocbs04 <profile.julio@gmail.com>
2021-09-06 19:57:22 -0300

    Finalizando do projeto em front-end com angular, não comitei nas várias aulas pq o projeto estava com erro(devido a service e router serem desnvoldidos na ultima aula testei está funcionando com o spring)

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-details',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-details.component.css']
 })
 export class RoomDetailsComponent implements OnInit {
+  id:number;
+  room: Room;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private roomService:RoomService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.room= new Room();
+    this.id= this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+    .subscribe(data => {
+      console.log(data)
+      this.room = data;
+    }, error => console.log(error));
   }
+
+  list(){
+    this.router.navigate(['rooms']);
+  }
+
+  
 
 }
